b71ba2bcf6832cfaae0fbbebcd938117ca737fc4
Hanyeol Cho <hanyeol.cho@gmail.com>
2020-09-25 21:22:35 +0900

    Rewrite with new code convention

--- index.js
+++ index.js
@@ -1,79 +1,79 @@
-GDriveSheets = (function() {
-    return {}
-})();
+var module = (function() {
+    return {
+        fetch_data: function(sheet_key, worksheet_id) {
+            var self = this;
 
-GDriveSheets.fetch_data = function(sheet_key, worksheet_id) {
-    var self = this;
-
-    return new Promise(function(resolve, reject) {
-        var url = self.get_feed_url(sheet_key, worksheet_id);
-	
-        fetch(url).then(function(response) {
-            if(response.ok) {
-                response.json().then(function(feed) {
-                    resolve(self.feed_to_data(feed));
+            return new Promise(function(resolve, reject) {
+                var url = self.get_feed_url(sheet_key, worksheet_id);
+            
+                fetch(url).then(function(response) {
+                    if(response.ok) {
+                        response.json().then(function(feed) {
+                            resolve(self.feed_to_data(feed));
+                        }, function() {
+                            reject();
+                        });
+                    } else {
+                        reject();
+                    }
                 }, function() {
                     reject();
                 });
-            } else {
-                reject();
+            });
+        },
+
+        feed_to_data: function(feed) {
+            var headers = this.get_headers(feed);
+            var entry = feed["feed"]["entry"].slice(headers.length);
+            var data = [], datum = {}, row = "2";
+        
+            for (var i = 0; i < entry.length; ++i) {
+                var cell = entry[i]["gs$cell"];
+        
+                if (cell["row"] !== row) {
+                    data.push(datum), datum = {}, row = cell["row"];
+                }
+        
+                var col = parseInt(cell["col"]);
+        
+                if (col - 1 < headers.length) {
+                    datum[headers[col - 1]] = entry[i]["content"]["$t"]
+                }
             }
-        }, function() {
-            reject();
-        });
-    });
-}
+        
+            if (datum) {
+                data.push(datum);
+            }
+        
+            return data;
+        },
 
-GDriveSheets.feed_to_data = function(feed) {
-    var headers = this.get_headers(feed);
-    var entry = feed["feed"]["entry"].slice(headers.length);
-    var data = [], datum = {}, row = "2";
+        get_headers: function(feed) {
+            var entry = feed["feed"]["entry"];
+            var headers = [];
+        
+            for (var i = 0; i < entry.length; i++) {
+                if (entry[i]["gs$cell"]["row"] !== "1") {
+                    break;
+                }
+        
+                headers.push(entry[i]["content"]["$t"]);
+            }
+        
+            return headers;
+        },
 
-    for (var i = 0; i < entry.length; ++i) {
-        var cell = entry[i]["gs$cell"];
-
-        if (cell["row"] !== row) {
-            data.push(datum), datum = {}, row = cell["row"];
-        }
-
-        var col = parseInt(cell["col"]);
-
-        if (col - 1 < headers.length) {
-            datum[headers[col - 1]] = entry[i]["content"]["$t"]
-        }
+        get_feed_url: function(sheet_key, worksheet_id) {
+            var url = "https://spreadsheets.google.com/feeds/cells/";
+        
+            url += sheet_key;
+            url += "/";
+            url += (worksheet_id || 1).toString();
+            url += "/public/full?alt=json";
+        
+            return url;
+        },
     }
+})();
 
-	if (datum) {
-    	data.push(datum);
-	}
-
-    return data;
-}
-
-GDriveSheets.get_headers = function(feed) {
-    var entry = feed["feed"]["entry"];
-    var headers = [];
-
-    for (var i = 0; i < entry.length; i++) {
-        if (entry[i]["gs$cell"]["row"] !== "1") {
-            break;
-        }
-
-        headers.push(entry[i]["content"]["$t"]);
-    }
-
-    return headers;
-}
-
-GDriveSheets.get_feed_url = function(sheet_key, worksheet_id) {
-    var url = "https://spreadsheets.google.com/feeds/cells/";
-
-    url += sheet_key;
-    url += "/";
-    url += (worksheet_id || 1).toString();
-    url += "/public/full?alt=json";
-
-    return url;
-}
-
-__MODULE__ = GDriveSheets;
+__MODULE__ = module;
