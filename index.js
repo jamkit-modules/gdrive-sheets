2c72eaaeeaef46d0207ae9371b22cbd39bef620d
Hanyeol Cho <hanyeol.cho@gmail.com>
2021-10-03 18:21:33 +0900

    Change for gdrive v3 deprecation

--- index.js
+++ index.js
@@ -1,79 +1,64 @@
 var module = (function() {
+
+    function _feed_to_data(feed) {
+        var headers = _get_headers(feed);
+        var rows = feed["table"]["rows"] || [];
+        var data = [], datum = {}, row = "2";
+    
+        for (var row = 1; row < rows.length; ++row) {
+            var entry = rows[row]["c"], datum = {};
+
+            for (var col = 0; col < entry.length; col++) {
+                datum[headers[col]] = entry[col]["v"];
+            }
+
+            data.push(datum);
+        }
+    
+        return data;
+    }
+
+    function _get_headers(feed) {
+        var rows = feed["table"]["rows"] || [];
+        var entry = rows.length > 0 ? rows[0]["c"] : [];
+        var headers = [];
+    
+        for (var col = 0; col < entry.length; col++) {
+            headers.push(entry[col]["v"]);
+        }
+    
+        return headers;
+    }
+
+    function _get_feed_url(spreadsheet_id, sheet_name) {
+        var url = "https://docs.google.com/spreadsheets/d/" + spreadsheet_id + "/gviz/tq?tqx=out:json";
+
+        if (sheet_name) {
+            url += "&" + "sheet=" + encodeURIComponent(sheet_name);
+        }
+     
+        return url;
+    }
+
     return {
-        fetch_data: function(sheet_key, worksheet_id) {
-            const self = this;
-
-            return new Promise(function(resolve, reject) {
-                var url = self.get_feed_url(sheet_key, worksheet_id);
+        fetch_data: function(spreadsheet_id, sheet_name) {
+            var url = _get_feed_url(spreadsheet_id, sheet_name);
             
-                fetch(url)
-                    .then(function(response) {
-                        if(response.ok) {
-                            return response.json();
-                        } else {
-                            return Promise.reject({ "status": response.status });
-                        }
-                    })
-                    .then(function(feed) {
-                        resolve(self.feed_to_data(feed));
-                    })
-                    .catch(function(error) {
-                        reject(error);
-                    });
-            });
-        },
-
-        feed_to_data: function(feed) {
-            var headers = this.get_headers(feed);
-            var entry = feed["feed"]["entry"].slice(headers.length);
-            var data = [], datum = {}, row = "2";
-        
-            for (var i = 0; i < entry.length; ++i) {
-                var cell = entry[i]["gs$cell"];
-        
-                if (cell["row"] !== row) {
-                    data.push(datum), datum = {}, row = cell["row"];
-                }
-        
-                var col = parseInt(cell["col"]);
-        
-                if (col - 1 < headers.length) {
-                    datum[headers[col - 1]] = entry[i]["content"]["$t"]
-                }
-            }
-        
-            if (datum) {
-                data.push(datum);
-            }
-        
-            return data;
-        },
-
-        get_headers: function(feed) {
-            var entry = feed["feed"]["entry"];
-            var headers = [];
-        
-            for (var i = 0; i < entry.length; i++) {
-                if (entry[i]["gs$cell"]["row"] !== "1") {
-                    break;
-                }
-        
-                headers.push(entry[i]["content"]["$t"]);
-            }
-        
-            return headers;
-        },
-
-        get_feed_url: function(sheet_key, worksheet_id) {
-            var url = "https://spreadsheets.google.com/feeds/cells/";
-        
-            url += sheet_key;
-            url += "/";
-            url += (worksheet_id || 1).toString();
-            url += "/public/full?alt=json";
-        
-            return url;
-        },
+            return fetch(url)
+                .then(function(response) {
+                    if (response.ok) {
+                        return response.text();
+                    } else {
+                        return Promise.reject({ "status": response.status });
+                    }
+                })
+                .then(function(text) {
+                    return JSON.parse(text.match(/\((\{.+\})\)/)[1]);
+                })
+                .then(function(feed) {
+                    return _feed_to_data(feed);
+                });
+        }
     }
 })();
 
