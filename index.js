abca024ce22daa41f4638863a3bd16dc81ce9c63
Hanyeol Cho <hanyeol.cho@gmail.com>
2023-09-01 22:11:10 +0900

    Use arrow function notation

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-var module = (function() {
-
+const module = (() => {
     function _feed_to_data(feed) {
-        var headers = _get_headers(feed);
-        var rows = feed["table"]["rows"] || [];
-        var data = [], datum = {}, row = "2";
+        const headers = _get_headers(feed);
+        const rows = feed["table"]["rows"] || [];
+        const data = [], datum = {}, row = "2";
     
-        for (var row = 1; row < rows.length; ++row) {
-            var entry = rows[row]["c"], datum = {};
+        for (let row = 1; row < rows.length; ++row) {
+            const entry = rows[row]["c"], datum = {};
 
-            for (var col = 0; col < entry.length; col++) {
+            for (let col = 0; col < entry.length; col++) {
                 if (headers[col]) {
                     datum[headers[col]] = (entry[col] || {})["v"] || "";
                 }
@@ -21,11 +20,11 @@ var module = (function() {
     }
 
     function _get_headers(feed) {
-        var rows = feed["table"]["rows"] || [];
-        var entry = rows.length > 0 ? rows[0]["c"] : [];
-        var headers = [];
+        const rows = feed["table"]["rows"] || [];
+        const entry = rows.length > 0 ? rows[0]["c"] : [];
+        const headers = [];
     
-        for (var col = 0; col < entry.length; col++) {
+        for (let col = 0; col < entry.length; col++) {
             headers.push(((entry[col] || {})["v"] || "").trim());
         }
     
@@ -33,7 +32,7 @@ var module = (function() {
     }
 
     function _get_feed_url(spreadsheet_id, sheet_name) {
-        var url = "https://docs.google.com/spreadsheets/d/" + spreadsheet_id + "/gviz/tq?tqx=out:json";
+        let url = "https://docs.google.com/spreadsheets/d/" + spreadsheet_id + "/gviz/tq?tqx=out:json";
 
         if (sheet_name) {
             url += "&" + "sheet=" + encodeURIComponent(sheet_name);
@@ -43,21 +42,21 @@ var module = (function() {
     }
 
     return {
-        fetch_data: function(spreadsheet_id, sheet_name) {
-            var url = _get_feed_url(spreadsheet_id, sheet_name);
+        fetch_data: (spreadsheet_id, sheet_name) => {
+            const url = _get_feed_url(spreadsheet_id, sheet_name);
             
             return fetch(url)
-                .then(function(response) {
+                .then((response) => {
                     if (response.ok) {
                         return response.text();
                     } else {
                         return Promise.reject({ "status": response.status });
                     }
                 })
-                .then(function(text) {
+                .then((text) => {
                     return JSON.parse(text.match(/\((\{.+\})\)/)[1]);
                 })
-                .then(function(feed) {
+                .then((feed) => {
                     return _feed_to_data(feed);
                 });
         }
